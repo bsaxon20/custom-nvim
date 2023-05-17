b10ac108884d0253be5917d8a9b66b73e6d624f4
Ben Saxon <bsaxon@mandmdirect.com>
2023-05-17 13:18:29 +0100

    Deleted the hard set lua line colour

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,11 +1,5 @@
 require"battery".setup({})
 require('lualine').setup({
     options = { theme = 'catpuccin-mocha' },
-    nvimbattery = {
-  nvimbattery = {
-  function()
-    return require("battery").get_status_line()
-  end,
-}}})
-
+})
 
